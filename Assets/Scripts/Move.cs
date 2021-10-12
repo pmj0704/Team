0293b69e49af0381472c8b93be753847bf8d2a57
pmj0704 <ggm.s210012@ggh.goe.go.kr>
2021-10-12 11:35:06 +0900

    병합함

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    [SerializeField]
+    private float walkSpeed;
+
+    [SerializeField]
+    private float JumpPower;
+
+    [SerializeField]
+    private float lookSensitivity;
+
+
+    [SerializeField]
+    private float cameraRotationLimit;
+    private float currentCameraRotationX = 0;
+
+    [SerializeField]
+    private Camera theCamera;
+
+    private Rigidbody myRigid;
+
+    private bool IsJumping;
+    private bool IsLaying= false;
+    private Vector3 LayPos;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        LayPos = new Vector3(0, -0.6f, 0);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Moving();
+        Jump();
+        CameraRotation();
+        CharacterRotation();
+        LayDown();
+    }
+
+    private void Moving()
+    {
+
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _moveDirX;
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
+
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!IsJumping)
+            {
+                IsJumping = true;
+                myRigid.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private void LayDown()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if(!IsLaying)
+            {
+                Debug.Log("Lay");
+                IsLaying = true;
+                theCamera.transform.position = new Vector3(theCamera.transform.position.x, -0.5f, theCamera.transform.position.z);
+            }
+            else
+            {
+                IsLaying = false;
+                theCamera.transform.position = new Vector3(theCamera.transform.position.x, 0f, theCamera.transform.position.z);
+            }
+        }
+    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Ground"))
+    //    {
+    //        IsJumping = false;
+    //    }
+    //}
+
+
+    private void CharacterRotation()
+    {
+        // 좌우 캐릭터 회전
+        float _yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
+        myRigid.MoveRotation(myRigid.rotation * Quaternion.Euler(_characterRotationY));
+        //Debug.Log(myRigid.rotation);
+        //Debug.Log(myRigid.rotation.eulerAngles);
+    }
+
+    private void CameraRotation()
+    {
+        // 상하 카메라 회전
+        float _xRotation = Input.GetAxisRaw("Mouse Y");
+        float _cameraRotationX = _xRotation * lookSensitivity;
+        currentCameraRotationX -= _cameraRotationX;
+        currentCameraRotationX = Mathf.Clamp(currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        theCamera.transform.localEulerAngles = new Vector3(currentCameraRotationX, 0f, 0f);
+    }
+
+}
