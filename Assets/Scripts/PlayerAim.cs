6ec35300d402d4fbc8a40c0ad02137d1a8512f7d
pmj0704 <ggm.s210012@ggh.goe.go.kr>
2021-10-19 10:54:56 +0900

    Aim보정

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class PlayerAim : MonoBehaviour
 {
-
-    private void Start()
-    {
-        Debug.Log("gj");
-    }
+    private bool Move = false;
+    Collider collider;
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log(other.gameObject.tag);
+        switch(other.gameObject.tag)
+        {
+            case "Drawer":
+                if(Input.GetKeyDown(KeyCode.F))
+                {
+                    collider = other;
+                    Move = true;
+                }
+                break;
+        }
+    }
+    private void Update()
+    {
+        if(Move)
+        {
+            if (collider.transform.position.x < 0.1f) collider.transform.position = Vector3.MoveTowards(collider.transform.position, new Vector3(0.5f, collider.transform.position.y, collider.transform.position.z), Time.deltaTime * 2);
+            else collider.transform.position = Vector3.MoveTowards(collider.transform.position, new Vector3(0.05f, collider.transform.position.y, collider.transform.position.z), Time.deltaTime * 2);
+
+            if (collider.transform.position.x < 0.051 || collider.transform.position.x > 0.49) Move = false;
+        }
     }
 }
