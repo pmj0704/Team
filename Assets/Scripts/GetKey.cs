11a2a3942d14309e2bc4834ee13bf8a0246d95ea
김유진 <yujin050420@gmail.com>
2021-10-19 11:53:46 +0900

    키인벤토리 1차 완성

--- Assets/Scripts/GetKey.cs
+++ Assets/Scripts/GetKey.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GetKey : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField]
+    private GameObject getKeyText;
+    [SerializeField]
+    private GameObject getkeyImage;
+
+    private bool inventorykey = false;
+    private bool inventoryText = false;
+
+    
+    private void OnTriggerStay(Collider other)
     {
-        //Debug.Log(other.gameObject.tag);
         if (other.gameObject.tag == "PlayerAim")
         {
+
+             inventoryText = true;
+             getKeyText.SetActive(inventoryText);
+            
             if (Input.GetKeyDown(KeyCode.F))
             {
                 Debug.Log("GetKey");
+                GameManager.Instance.hasKey = true;
+                inventorykey = true;
+                getkeyImage.SetActive(inventorykey);
+              
+                if(inventorykey == true)
+                {
+                    inventoryText = false;
+                    getKeyText.SetActive(inventoryText);
+                }
             }
         }
     }
 }
+
+
+
+
--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -6,6 +6,7 @@ public class PlayerAim : MonoBehaviour
 {
     private bool Move = false;
     Collider collider;
+
     private void OnTriggerStay(Collider other)
     {
         switch(other.gameObject.tag)
@@ -23,10 +24,18 @@ public class PlayerAim : MonoBehaviour
     {
         if(Move)
         {
-            if (collider.transform.position.x < 0.1f) collider.transform.position = Vector3.MoveTowards(collider.transform.position, new Vector3(0.5f, collider.transform.position.y, collider.transform.position.z), Time.deltaTime * 2);
-            else collider.transform.position = Vector3.MoveTowards(collider.transform.position, new Vector3(0.05f, collider.transform.position.y, collider.transform.position.z), Time.deltaTime * 2);
+            if (collider.transform.position.x < 0.1f) collider.transform.position 
+                    = Vector3.MoveTowards(collider.transform.position, 
+                    new Vector3(0.5f, collider.transform.position.y, 
+                    collider.transform.position.z), Time.deltaTime * 2);
 
-            if (collider.transform.position.x < 0.051 || collider.transform.position.x > 0.49) Move = false;
+            else collider.transform.position = 
+                    Vector3.MoveTowards(collider.transform.position, 
+                    new Vector3(0.05f, collider.transform.position.y, 
+                    collider.transform.position.z), Time.deltaTime * 2);
+
+            if (collider.transform.position.x < 0.051 || collider.transform.position.x > 0.49) 
+                Move = false;
         }
     }
 }
