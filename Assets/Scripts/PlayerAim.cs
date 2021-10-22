103c5f549c399e25100b11e4d5cb227b0f2f3dea
pmj0704 <ggm.s210012@ggh.goe.go.kr>
2021-10-22 21:46:09 +0900

    no message

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -8,25 +8,19 @@ public class PlayerAim : MonoBehaviour
     Collider collider;
     private void OnTriggerStay(Collider other)
     {
+        Debug.Log(other.gameObject.tag);
         switch(other.gameObject.tag)
         {
             case "Drawer":
                 if(Input.GetKeyDown(KeyCode.F))
                 {
-                    collider = other;
-                    Move = true;
+
                 }
                 break;
         }
     }
-    private void Update()
+    private void Open()
     {
-        if(Move)
-        {
-            if (collider.transform.position.x < 0.1f) collider.transform.position = Vector3.MoveTowards(collider.transform.position, new Vector3(0.5f, collider.transform.position.y, collider.transform.position.z), Time.deltaTime * 2);
-            else collider.transform.position = Vector3.MoveTowards(collider.transform.position, new Vector3(0.05f, collider.transform.position.y, collider.transform.position.z), Time.deltaTime * 2);
-
-            if (collider.transform.position.x < 0.051 || collider.transform.position.x > 0.49) Move = false;
-        }
+        while()
     }
 }
